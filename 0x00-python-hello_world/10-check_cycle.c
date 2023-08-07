da3e363276ad7ad64c0b2c8596271c9406d25101
Mona Lisa <125455854+swervolee@users.noreply.github>
2023-08-07 21:52:00 +0300

    checking for a cycle in a linked lists

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,7 +6,40 @@
  */
 
 int check_cycle(listint_t *list)
-{
+	{
+	listint_t *p2;
+	listint_t *prev;
+
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
+	{
+		list = list->next;
+		p2 = p2->next->next;
+
+		if (list == p2)
+		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
+		}
+	}
+
+	return (0);
+}
+/*{
 	int rtn = 0;
 	listint_t *check1 = list, *check2 = list;
 
@@ -24,3 +57,4 @@ int check_cycle(listint_t *list)
 
 	return (rtn);
 }
+*/
