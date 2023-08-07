7013e4940c89971e4a7418b146d678a15d05b96c
Mona Lisa <125455854+swervolee@users.noreply.github>
2023-08-07 22:23:05 +0300

    checking for a cycle in a linked lists

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,60 +1,25 @@
 #include "lists.h"
+
 /**
- *check_cycle - checks for a cycle in a linked list
+ *check_cycle -  checks for a cycle
  *@list: the linked list to check
- *Return: 1 if cycle is present else 0
+ *Return: 1 if a cycle is found else 0
  */
 
 int check_cycle(listint_t *list)
-	{
-	listint_t *p2;
-	listint_t *prev;
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	p2 = list;
-	prev = list;
-	while (list && p2 && p2->next)
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
-		list = list->next;
-		p2 = p2->next->next;
-
-		if (list == p2)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 		{
-			list = prev;
-			prev =  p2;
-			while (1)
-			{
-				p2 = prev;
-				while (p2->next != list && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == list)
-					break;
-
-				list = list->next;
-			}
 			return (1);
 		}
 	}
 
 	return (0);
 }
-/*{
-	int rtn = 0;
-	listint_t *check1 = list, *check2 = list;
-
-	while (check1 && check2 != NULL)
-	{
-		check1 = check1->next;
-		check2 = check2->next->next;
-
-		if (check1 == check2)
-		{
-			rtn = 1;
-			break;
-		}
-	}
-
-	return (rtn);
-}
-*/
