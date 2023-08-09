479661b0d839f2047aeac7017f46b256e1fec33f
Mona Lisa <125455854+swervolee@users.noreply.github>
2023-08-09 15:19:03 +0300

    correction on question 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ else:
 if (d > 5):
     print("Last digit of {} is {} and is greater than 5".format(number, d))
 elif (d == 0):
-    print("Last digit of {} is {} and is zero".format(number, d))
+    print("Last digit of {} is {} and is 0".format(number, d))
 else:
-    print("Last digit of {} is {} and is less than 6 nd not 0".format(number, d))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, d))
