ecc40464c15879e2baa1d92a64811969347dcb7b
Mona Lisa <125455854+swervolee@users.noreply.github>
2023-08-09 15:08:17 +0300

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-if (number >= 0):
-    d = number % 10
-else:
+if (number < 0):
     d = (number * -1) % 10
-
+else:
+    d = number % 10
 if (d > 5):
     print("Last digit of {} is {} and is greater than 5".format(number, d))
 elif (d == 0):
-          print("Last digit of {} is {} and is 0".format(number, d))
+    print("Last digit of {} is {} and is zero".format(number, d))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, d))
+    print("Last digit of {} is {} and is less than 6 nd not 0".format(number, d))
