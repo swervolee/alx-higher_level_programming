13dd2b949474765cf523d1af9a359ebd0bce1e22
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-10 18:20:36 +0300

    correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in dir(hidden_4):
-        if name[0] != '_':
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
             print(name)
