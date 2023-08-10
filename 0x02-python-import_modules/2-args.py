733a305fccffa241fca6a0499297e06dbca691b6
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-10 18:34:27 +0300

    an update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
     leny = len(sys.argv)
     if (leny == 2):
-        print("{} arguement".format(leny - 1))
+        print("{} argument".format(leny - 1))
     elif (leny == 1):
-        print("{} arguements.".format(leny - 1))
+        print("{} arguments.".format(leny - 1))
     else:
-        print("{} arguements:".format(leny - 1))
+        print("{} arguments:".format(leny - 1))
 
     if ((leny - 1) >= 1):
         i = 0
