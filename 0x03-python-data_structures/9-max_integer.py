2baf50718220dea0545de9bc773a66d62d16be5f
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-12 20:49:48 +0300

    correctin

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,12 +2,13 @@
 def max_integer(my_list=[]):
     length = len(my_list)
 
-    if (length == 0):
-        return(None)
-    else:
-        greatest = my_list[0]
+    if length == 0:
+        return (None)
 
-        for i in range(length):
-            if (my_list[i] > greatest):
-                greatest = my_list[i]
-    return (greatest)
+    max_int = my_list[0]
+
+    for i in range(1, length):
+        if my_list[i] > max_int:
+            max_int = my_list[i]
+
+    return (max_int)
