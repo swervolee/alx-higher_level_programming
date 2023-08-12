a0b85ebe39c25b83ddec22d16b01feb553d1ee73
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-12 14:16:47 +0300

    printing a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
             if (j != 0):
                 print(" ", end="")
             print("{:d}".format(j), end="")
