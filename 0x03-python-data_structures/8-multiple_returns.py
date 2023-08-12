cbcc2cbb550e2e5b91839889c6974150e918d2c4
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-12 18:21:43 +0300

    creating a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
 
     if (len == 0):
-        return (0, None)
+        return (length, None)
 
-    return (0, sentence[0])
+    return (length, sentence[0])
