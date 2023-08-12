afd522550c066ada750477ee737b060b87175004
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-12 09:05:47 +0300

    correction on questin 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.sort(reverse=True)
-    for i in range(len(my_list)):
+    i = len(my_list) - 1
+    while (i >= 0):
         print("{:d}".format(my_list[i]))
+        i -= 1
