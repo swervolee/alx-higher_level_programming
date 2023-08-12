a7fe2fc155294f6ec1797db2220055872c9a0efd
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-12 18:58:54 +0300

    finding the greatest

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+
+    if (length == 0):
+        return(None)
+    greatest = 0
+
+    for i in range(length):
+        if (my_list[i] > greatest):
+            greatest = my_list[i]
+    return (greatest)
