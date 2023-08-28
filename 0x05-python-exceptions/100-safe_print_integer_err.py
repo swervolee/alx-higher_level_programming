47dc97e87dfca4bcee8d196844621b01b36c77ee
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-28 16:18:22 +0300

    a fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-import sys
-
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as e:
+        import sys
         sys.stderr.write("Exception: {}\n".format(e))
         return (False)
     return (True)
