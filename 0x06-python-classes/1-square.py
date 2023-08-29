6fc78041c111ebf13352bb18f2833163f5288b86
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-29 14:52:41 +0300

    instanciating size in the class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""creates a class square"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size):
+        """instanciates size"""
+
+        self.__size = size
