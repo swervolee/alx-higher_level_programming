dc7d6e1868e870258c01f5c43d6485e48d1181f9
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-29 15:19:36 +0300

    finding the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""a class that defines a square"""
+
+
+class Square:
+    """adds exception to the class square"""
+
+    def __init__(self, size=0):
+
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
