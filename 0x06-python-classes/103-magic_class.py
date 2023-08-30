aeb259b2fdea3e3858fb0c6763531cc82bbf9964
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-30 16:27:47 +0300

    a fix

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,15 +8,15 @@ import math
 class MagicClass:
     """defines models on the circle eg area and circumfrence"""
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self.__radius = 0
 
         if type(radius) is not int or type(radius) is not float:
-            raise TypeError("Radius must be int or float")
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        return (self.__radius ** 2 * (math.pi))
+        return (self.__radius ** 2 * math.pi)
 
-    def circumfrence(self):
+    def circumference(self):
         return (2 * math.pi * self.__radius)
