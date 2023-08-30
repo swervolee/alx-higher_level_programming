a699708c2ca19ed85b53d21880fa7c1ed5c713fb
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-08-30 16:30:06 +0300

    a fix

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,7 +11,7 @@ class MagicClass:
     def __init__(self, radius=0):
         self.__radius = 0
 
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
