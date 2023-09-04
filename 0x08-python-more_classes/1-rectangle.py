c9c19cc1fb72c16347c13fd15fd2a74951c388ba
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-04 18:12:41 +0300

    a fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 
 class Rectangle:
     """
-    a class rectangle with properties
+    A class rectangle with properties
     length
     width
     """
@@ -17,6 +17,18 @@ class Rectangle:
     def width(self):
         return self.__width
 
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -24,15 +36,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    @property
-    def height(self):
-        return self.height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__height = value
