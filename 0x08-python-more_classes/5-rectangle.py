2100b44606421ca18774d8bca35bd496ddca8a88
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-04 20:13:11 +0300

    correction

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -57,4 +57,4 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        print("Bye rectangle")
+        print("Bye rectangle...")
