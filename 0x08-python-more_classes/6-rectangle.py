bc5b470ad91f349f8bd44551ca0f7bba818bba75
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-04 20:24:23 +0300

    a public instance

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+#!/usr/bin/python3
 """this is a module that creates a class rectangle"""
 
 
@@ -8,6 +10,8 @@ class Rectangle:
     length
     width
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
