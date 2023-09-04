2440f7287a4bedd8f92cb49aedf135ef3b438f95
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-04 18:25:12 +0300

    calculating perimeter and area

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+
+"""this is a module that creates a class rectangle"""
+
+
+class Rectangle:
+    """
+    A class rectangle with properties
+    length
+    width
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return self.__height * self.width
