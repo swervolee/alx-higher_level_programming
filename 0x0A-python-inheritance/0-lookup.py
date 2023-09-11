a3def1ff47c384aa31d83fbe4fc8ed2600c1d999
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-11 12:53:46 +0300

    added comments

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+a module to print the  attributes of a python clas
+"""
+
 
 def lookup(obj):
+    """
+    a function to list a classes attributes
+
+    args:
+
+    @obj:
+    the object input
+
+    @return:
+    a list of the objects attributes
+    """
     return (dir(obj))
