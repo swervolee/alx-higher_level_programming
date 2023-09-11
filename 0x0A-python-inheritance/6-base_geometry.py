db8861efd15c24797d9a46d55dfb45e2a27ab828
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-11 16:51:34 +0300

    creates  a new method area that always raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+a module to create a class
+"""
+
+
+class BaseGeometry:
+    """
+    a new class
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        """
+        a method to calculate area
+        """
+        raise Exception("area() is not implimented")
