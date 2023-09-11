4baf9eaec8867c69f04524a04b1021b934892ebb
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-11 15:01:56 +0300

    inheritance from a list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a module to sort a list
+"""
+
+
+class MyList(list):
+    """
+    a class with a method that sorts a list
+    """
+
+    def print_sorted(self):
+        print(sorted(self))
