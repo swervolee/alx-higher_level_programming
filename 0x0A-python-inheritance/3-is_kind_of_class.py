d652972fc33a5042524d0b04d96c0af0eb11f8d7
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-11 15:52:12 +0300

    checking for both class and subclass

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,6 +7,6 @@ tells if an object is derived or is from a given class
 def is_kind_of_class(obj, a_class):
     """
     if obj is an instance of or if the object is an instance of class inherited
-from the specified class returb True else return false
+    from the specified class returb True else return false
     """
     return (isinstance(obj, a_class))
