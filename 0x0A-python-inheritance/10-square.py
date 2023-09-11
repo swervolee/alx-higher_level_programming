b35aff38b073abf969fb5252de98b3f800264c51
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-12 02:37:57 +0300

    corrected some comment

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,6 +5,8 @@ Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
+    """defines a square
+    """
     def __init__(self, size):
         """innitializes the square"""
         super().__init__(size, size)
