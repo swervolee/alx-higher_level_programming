fd4462531498421f7024ca876525b2ad825ab0fc
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-13 00:41:29 +0300

    saving an object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -18,4 +18,4 @@ def save_to_json_file(my_obj, filename):
     import json
     j_object = json.dumps(my_obj)
     with open(filename, mode="w", encoding="utf-8") as file:
-        json.dump(j_object, file)
+        json.dump(my_obj, file)
