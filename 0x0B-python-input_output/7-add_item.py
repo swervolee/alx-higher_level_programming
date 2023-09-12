1e177d244db75619cfdb18f5a91128bb8bd6108b
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-13 02:33:59 +0300

    handling json files

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A module to read input, put it into a list,
+convert the list to JSON, and store it in a file.
+"""
+
+
+if __name__ == "__main__":
+    """
+    continously appends to a json file
+    """
+    from sys import argv
+    import json
+    load_from_json = __import__("6-load_from_json_file").load_from_json_file
+    save_to_json = __import__("5-save_to_json_file").save_to_json_file
+
+    new = []
+
+    for a in argv[1:]:
+        new.append(a)
+
+    try:
+        old = load_from_json("add_new_item.json")
+        save_to_json(new + old, "add_new_item.json")
+    except FileNotFoundError:
+        save_to_json(new, "add_new_item.json")
