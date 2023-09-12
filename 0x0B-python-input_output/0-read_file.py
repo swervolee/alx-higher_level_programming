64c219b5c5fdbdfeb3d7c9e656f28244398db9cf
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-12 18:49:15 +0300

    a function that reads and writes to the std from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a module to  read  a file and print it to the standard output
+"""
+
+
+def read_file(filename=""):
+    """
+    a function to read and print a file
+
+    Args:
+            filename - the path to the file
+
+    return:
+            nothing
+    """
+    with open(filename, mode="r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
