c3d150d64abca9a572dab0e0487ae455066f0de5
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-13 11:33:59 +0300

    code fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,19 +4,16 @@ A module to read input, put it into a list,
 convert the list to JSON, and store it in a file.
 """
 
+import sys
+
 if __name__ == "__main__":
-    from sys import argv
-    import json
-    load_from_json = __import__("6-load_from_json_file").load_from_json_file
-    save_to_json = __import__("5-save_to_json_file").save_to_json_file
-
-    new = []
-
-    for a in argv[1:]:
-        new.append(a)
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        old = load_from_json("add_new_item.json")
-        save_to_json(old + new, "add_new_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        save_to_json(new, "add_new_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
