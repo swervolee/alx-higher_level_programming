4b4440029639412194dcb40002c29eca9ec3c210
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-13 11:53:08 +0300

    correction on number seven

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,16 +5,16 @@ convert the list to JSON, and store it in a file.
 """
 
 
-
 if __name__ == "__main__":
-    import sys
-    save_to_json = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json = \
-        __import__('6-load_from_json_file').load_from_json_file
+    from sys import argv
+    load_from_json = __import__("6-load_from_json_file").load_from_json_file
+    save_to_json = __import__("5-save_to_json_file").save_to_json_file
 
     try:
-        items = load_from_json("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json(items, "add_item.json")
+        new =  load_from_json("add_item.json")
+    except:
+        new = []
+
+    new.extend(argv[1:])
+
+    save_to_json(new, "add_item.json")
