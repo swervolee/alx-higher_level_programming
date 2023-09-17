a1bfffafbee0828489495ccfe948c0140553ba7e
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-17 12:10:47 +0300

    adding json features to the base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,29 @@ class Base:
         if not dc or len(dc) == 0:
             return "[]"
         return (json.dumps(dc))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        takes a list of objects that inherit from base
+        and gets a dictionary representation of those objects
+        then changes those dictionary objects to a json representation
+        and finally writes that data to a file
+        """
+        File = cls.__name__ + ".json"
+        with open(File, mode="w", encoding="utf-8") as file:
+            if list_objs is None or len(list_objs) == 0:
+                file.write("[]")
+            else:
+                dc = [a.to_dictionary() for a in list_objs]
+                file.write(Base.to_json_string(dc))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        deserializes a json string
+        """
+        if json_string == "[]" or json_string is None:
+            return "[]"
+        else:
+            return (json.loads(json_string))
