c9cd7947654baee6f8a7cb467c04f110b8124c77
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-02 18:46:14 +0300

    a fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,32 +7,32 @@ prints the following statistics:
     - Count of read status codes up to that point.
 """
 
+if __name__ == "__main__":
+    import sys
 
-import sys
+    size = 0
+    i = 1
 
-size = 0
-i = 1
+    sc = [200, 301, 400, 401, 403, 404, 405, 500]
+    sc.sort()
 
-sc = [200, 301, 400, 401, 403, 404, 405, 500]
-sc.sort()
+    ls = []
+    for line in sys.stdin:
+        try:
+            l = line.split()
+            size += int(l[-1])
+            ls.append(int(l[-2]))
 
-ls = []
-for line in sys.stdin:
-    try:
-        l = line.split()
-        size += int(l[-1])
-        ls.append(int(l[-2]))
-
-        if i == 10:
-            i = 1
+            if i == 10:
+                i = 1
+                print(f"File size: {size}")
+                for k in sc:
+                    if k in ls:
+                        print("{}: {}".format(k, ls.count(k)))
+            else:
+                i += 1
+        except KeyboardInterrupt:
             print(f"File size: {size}")
             for k in sc:
                 if k in ls:
                     print("{}: {}".format(k, ls.count(k)))
-        else:
-            i += 1
-    except KeyboardInterrupt:
-        print(f"File size: {size}")
-        for k in sc:
-            if k in ls:
-                print("{}: {}".format(k, ls.count(k)))
