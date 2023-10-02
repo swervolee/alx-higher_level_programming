266bbfbb9444ea781493fa47fd270be950ef2774
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-02 18:26:12 +0300

    apache log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-#!/usr/bin/python3
 """Reads from standard input and computes metrics.
 
 After every ten lines or the input of a keyboard interruption (CTRL + C),
@@ -10,51 +8,25 @@ prints the following statistics:
 """
 
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics.
+import sys
 
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+size = 0
+i = 1
 
+sc = [200, 301, 400, 401, 403, 404, 405, 500]
+sc.sort()
 
-if __name__ == "__main__":
-    import sys
+ls = []
+for line in sys.stdin:
+    l = line.split()
+    size += int(l[-1])
+    ls.append(int(l[-2]))
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
-
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
-
-            line = line.split()
-
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
+    if i == 10:
+        i = 1
+        print(f"File size: {size}")
+        for k in sc:
+            if k in ls:
+                print("{}: {}".format(k, ls.count(k)))
+    else:
+        i += 1
