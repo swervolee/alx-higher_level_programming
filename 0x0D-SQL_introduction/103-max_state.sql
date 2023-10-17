7d5adee0199bee5719a86c7effe22518730ee65a
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-18 00:42:27 +0300

    Listing the max temperature in each state

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 -- Displays the max temperature of each state
 
-select state, MAX(values) as max_temp
+select state, MAX(value) as max_temp
 FROM  temperatures
 GROUP BY state
-ORDER BY state ASC
+ORDER BY state ASC;
