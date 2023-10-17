166a5a271d2cc352bc0870ffc11c902e2c277cb5
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-18 00:29:20 +0300

    Listing top 3 cities with high than average temperatur

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Importing and using a Dump table
 
-SELECT `city`, AVG(`value`) as `avg_temp`
-WHERE MONTH = `7` OR MONTH = `8`
-GROUP BY `city`
-ORDER BY `avg_temp`
-LIMIT `3`
+SELECT city, AVG(value) as avg_temp
+WHERE MONTH = 7 OR MONTH = 8
+GROUP BY city
+ORDER BY avg_temp
+LIMIT 3;
