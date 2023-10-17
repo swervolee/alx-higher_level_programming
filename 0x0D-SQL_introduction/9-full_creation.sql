5b0bd3ae36cac1d4cbb2abadc4ef6681afb95bfb
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-17 20:15:39 +0300

    Creating a new table and inserting values into it

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Create a second table in hbtn_0c_0 called second_table
+-- The second_table description:
+--				id INT
+--				name VARCHAR(256)
+--				score INT
+
+CREATE TABLE IF NOT EXISTS second_table (
+id INT,
+name VARCHAR(256),
+score INT);
+
+INSERT INTO second_table
+VALUES (id = 1, name = 'John', score = 10), (id = 2, name = 'Alex', score = 3),
+(id = 3, name = 'Bob', score = 14), (id = 4, name = 'George', score = 8);
