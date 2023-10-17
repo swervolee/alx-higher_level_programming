2c72bf1b8ae422198843184a7109faa32b6b1c2d
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-17 20:28:57 +0300

    Creating and inserting into a table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -9,6 +9,11 @@ id INT,
 name VARCHAR(256),
 score INT);
 
-INSERT INTO second_table
-VALUES (id = 1, name = 'John', score = 10), (id = 2, name = 'Alex', score = 3),
-(id = 3, name = 'Bob', score = 14), (id = 4, name = 'George', score = 8);
+INSERT INTO second_table (id, name, score)
+VALUES(1, "John", 10);
+INSERT INTO second_table (id, name, score)
+VALUES(2, "Alex", 3);
+INSERT INTO second_table (id, name, score)
+VALUES(3, "Bob", 14);
+INSERT INTO second_table (id, name, score)
+VALUES( 4, "George", 8);
