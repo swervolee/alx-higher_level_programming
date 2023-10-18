3cbcb6fcf33370fd11b97bbb6ea565432d48b512
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-19 02:03:06 +0300

    Listing dexter show genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,7 @@ SELECT gnr.name
        FROM tv_genres AS gnr
        INNER JOIN tv_show_genres AS tsg
        	     ON gnr.id = tsg.genre_id
-       INNER JOIN tv_shows as ts
+       INNER JOIN tv_shows AS ts
        	     ON  ts.id = tsg.show_id
 	WHERE ts.title = "Dexter"
 	ORDER BY gnr.name
