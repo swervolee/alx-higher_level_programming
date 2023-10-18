56f06244811bedaae41dede3635d806ee89d3431
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-19 00:29:29 +0300

    Listing tv shows using join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- Lists all the shows by order of tvshow title and tvshow genre id
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-       FROM hbtn_0d_tvshows
+       FROM tv_shows
        INNER JOIN tv_show_genres
        	     ON tv_shows.id = tv_show_genres.show_id
        ORDER BY tv_shows.title, tv_show_genres.genre_id
