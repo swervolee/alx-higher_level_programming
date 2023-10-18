bc3e034187532f22c1784e99e2f9d9426f724896
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-18 20:04:19 +0300

    Granting all priviliges to two users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Gives all privileges to two users
 
-SHOW GRANT FOR 'user_0d_1'@'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
-SHOW GRANT FOR 'user_0d_2'@'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
