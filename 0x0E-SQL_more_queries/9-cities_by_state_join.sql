ec378588e4a4b035e784518214b2434547330331
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-19 00:05:53 +0300

    Joining cities and states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,5 +4,5 @@
 SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
        FROM `cities`
        INNER JOIN `states`
-       on `cities`.`state_id` = `states`.`id`
+       ON `cities`.`state_id` = `states`.`id`
        ORDER BY `cities`.`id` ASC;
