87e84ad8bac2dd9a10cad5eb90b6751d8f3e513a
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-18 22:03:55 +0300

    Creating a Database and a Table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -7,7 +7,7 @@ CREATE DATABASE
 
 CREATE TABLE
        IF NOT EXISTS
-       `hbtn_0d_usa.states`(
+       `hbtn_0d_usa`.`states`(
        PRIMARY KEY(`id`),
        `id` INT NOT NULL AUTO_INCREMENT,
        `name` VARCHAR(256) NOT NULL);
