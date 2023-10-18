9b37f64305c3c1ade4846ac18afdb9a841f0cdf9
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-19 01:04:49 +0300

    Counts genres

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all the genres of the tvshow database
+
+SELECT gnr AS 'genre' count(*) AS  'number_of_shows'
+       FROM tv_genres AS gnr
+       	    INNER JOIN tv_show_genres AS tsg
+	    ON gnr.id = tsg.id
+      GROUP BY gnr.name
+      ORDER BY number_of_shows DESC
