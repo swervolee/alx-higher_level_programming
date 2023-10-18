0c6ca4c9c0d81b61d183cbe36e862271d757c6a6
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-18 20:26:19 +0300

    Granting permission to a user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
 -- Grants a user all privileges if user exists
 
-IF NOT EXISTS 'user_0d_1'@'localhost'
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO  'user_0d_1'@'localhost';
+CREATE USER
+       IF NOT EXISTS 'user_0d_1'@'localhost'
+       IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+      ON *.*
+      TO 'user_0d_1'@'localhost';
