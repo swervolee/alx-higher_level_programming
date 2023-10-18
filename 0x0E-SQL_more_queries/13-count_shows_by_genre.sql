b835286ffe7edc03e512b9b712fe6f3b089cb450
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-19 01:08:22 +0300

    Counts genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@
 SELECT gnr AS 'genre' count(*) AS  'number_of_shows'
        FROM tv_genres AS gnr
        	    INNER JOIN tv_show_genres AS tsg
-	    ON gnr.id = tsg.id
+	    ON gnr.id = tsg.genre_id
       GROUP BY gnr.name
       ORDER BY number_of_shows DESC
