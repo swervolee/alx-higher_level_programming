366c75f33ddd6a41c1afcb3e18aaddcde407d835
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-10-18 20:08:48 +0300

    Granting permission to a user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Gives all privileges to two users
+-- Lists the privileges of two users
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Grants a user all privileges if user exists
+
+GRANT ALL PRIVILEGES ON *.* TO  'user_0d_1'@'localhost' IF EXISTS;
