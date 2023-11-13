74731fa70b261b61420c8a8661385323fdad1edd
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-11-14 00:42:17 +0300

    correction

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,6 @@
 #!usr/bin/node
-exports.callMeMoby = function (x, thefunction) {
-  for (let i = 0; i < x; i++) { thefunction(); }
+exports.callMeMoby = function (x, theFunction) {
+    for (let i = 0; i < x; i++) {
+	theFunction();
+    }
 };
