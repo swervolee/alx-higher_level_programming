d33219da8d0d9604eb01d16073734889deaff4f0
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-11-14 21:15:53 +0300

    Changes printing patterns

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./4-rectangle.js') {
+    constructor(size) {
+        super(size, size);
+    }
+
+    charPrint(c) {
+        if (c === undefined) {
+            this.print();
+        } else {
+
+            for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+	}
+    }
+};
