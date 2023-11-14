d0696a064a4ed84cc27a2bb66efb8fc39278632b
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-11-14 21:16:24 +0300

    correction

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./4-rectangle.js') {
-    constructor(size) {
-        super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint(c) {
-        if (c === undefined) {
-            this.print();
-        } else {
-
-            for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
+  }
 };
