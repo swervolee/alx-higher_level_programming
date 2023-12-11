e0d3df0bae8b795fa83ebca8d8eb3cc69791a33b
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-11 19:26:41 +0300

    Printing using loop

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = Math.floor(Number(process.argv[2]));
+
+if (isNaN(arg)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < arg; i++) {
+    console.log('C is fun');
+  }
+}
