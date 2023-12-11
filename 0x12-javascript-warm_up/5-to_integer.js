8d1729dbbe4ddcc46de30e222d2f1ba22265a42e
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-11 18:20:34 +0300

    correction

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,4 +2,8 @@
 
 const arg = Number(process.argv[2]);
 
-if (!isNaN(arg)) { console.log(`My number: ${arg}`); }
+if (!isNaN(arg)) {
+    console.log(`My number: ${Math.floor(arg)}`);
+} else {
+    console.log('Not a number');
+}
