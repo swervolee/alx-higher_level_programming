74e413a12dd36dc7d207c8d8b29b936426d5efcd
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-11 17:45:34 +0300

    concatenating

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-let args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
 if (args[0] === undefined) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    console.log(args[0]);
+  console.log(args[0]);
 }
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const a = args[0];
+const b = args[1];
+
+console.log(`${a} is ${b}`);
