8bdee7d3c4b2472ab86f9bc09ae6a4a2ce27cb9c
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-12 23:18:18 +0300

    Adds a printing option using a custom pattern

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        let line = '';
+        for (let j = 0; j < this.width; j++) {
+          line = line + c;
+        }
+        console.log(line);
+      }
+    } else {
+      super.print();
+    }
+  }
+};
