a534b3ab4279185db71487a7b2c767bf134398c5
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-12 22:54:34 +0300

    Adds the methods double and rotate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line = line + 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
