01bfb464d411ea6b840e0248cae24e627412923d
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-15 13:23:05 +0300

    question 3 correction

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-
 """makes a query using given search"""
+
 
 import MySQLdb
 import sys
 
-myuser, password, database, search = sys.argv[1:]
+if __name__ == '__main__':
+    myuser, password, database, search = sys.argv[1:]
 
-search = search.split(' ')[0]
+    search = search.split(' ')[0]
 
-db = MySQLdb.connect(host="localhost", user=myuser,
-                     passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", user=myuser,
+                         passwd=password, db=database)
 
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute(f"SELECT * FROM states WHERE name='{search}' ORDER BY id")
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id"
 
-rows = cur.fetchall()
+    cur.execute(query, (search,))
 
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
