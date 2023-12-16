06576878f0f11d2495dce6552a51075dda15e265
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-16 12:05:49 +0300

    fetches the first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Makes a query that selects the first state"""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine, text
+from model_state import Base, State
+import sys
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[
+                               3], pool_pre_ping=True))
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(engine)
+
+    session = Session()
+
+    for item in session.query(State).order_by(State.id):
+        print(item.id, item.name, sep=": ")
+        break;
