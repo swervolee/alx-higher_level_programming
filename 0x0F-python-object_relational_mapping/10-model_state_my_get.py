1f9cc2fd9b41b77007ade5f79946bee3699c172c
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-17 17:06:59 +0300

    Making a query search

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Selects all states that have the letter 'a'"""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine, text
+from model_state import Base, State
+import sys
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[
+                               3], pool_pre_ping=True))
+    search = sys.argv[4]
+
+    length = 0
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(engine)
+
+    session = Session()
+
+    result = session.query(State).filter(State.name == search).all()
+
+    print(len(result))
