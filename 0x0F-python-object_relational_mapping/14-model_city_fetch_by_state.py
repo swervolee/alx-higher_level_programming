5f0e8126c34435ee870e961a220dde57fa68a447
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-17 22:29:32 +0300

    creates a table City and makes a query of cities according to their states

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""makes a joined query between city and state"""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine, text
+from model_state import Base, State
+from model_city import City
+import sys
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[
+                               3], pool_pre_ping=True))
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(engine)
+
+    session = Session()
+
+    for st, ct in session.query(State, City).filter(
+            State.id == City.state_id).order_by(City.id):
+        print(f'{st.name}: ({st.id}) {ct.name}')
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Connects to the table city using sqlalchemy"""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+from sqlalchemy import ForeignKey
+
+class City(Base):
+    """Defines the table city"""
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
