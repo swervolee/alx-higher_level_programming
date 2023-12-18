80b7ea68dbdc55c203b457e7c44ab7e3a2d5fa41
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-18 10:12:43 +0300

    creating a relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Connects to the table states using sqlalchemy"""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Defines the table State"""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade='all, delete, delete-orphan')
