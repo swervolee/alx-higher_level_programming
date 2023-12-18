8eef7d57f5d52f9ec6db8fa09d96411cbd409483
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-18 14:50:58 +0300

    printing States and their cities using relationship"

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+
+import sqlalchemy
+from sqlalchemy import create_engine, text
+from relationship_state import Base, State
+import sys
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[
+                               3], pool_pre_ping=True))
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(engine)
+
+    session = Session()
+
+    for st in session.query(State).filter(
+            State.id == City.state_id).order_by(State.id, City.id):
+        print(st.id, st.name, sep=': ')
+
+        for ct in st.cities:
+            print('    ' + str(ct.id), ct.name, sep=': ')
