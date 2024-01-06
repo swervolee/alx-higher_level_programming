052087c4c7fe95e49cb3bb3a28f28fc2f9de5218
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-07 00:18:12 +0300

    prints request-id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@
 import sys
 import urllib.request
 
-
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
