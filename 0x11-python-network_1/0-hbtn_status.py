a729f2fa5bf6dc542d6ee0d398f90a9b535efd87
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-06 23:50:26 +0300

    Get with urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import urllib.request
 
-with urllib.request.urlopen('http://alx-intranet.hbtn.io/status') as resp:
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
     body = resp.read()
 
 print(f'Body response:\n\t- type: {type(body)}\n\t- content: {body}')
