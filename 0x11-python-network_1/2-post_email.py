7a5b926d452a0f2f7e253fa905fad5c647aeee00
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-07 00:57:56 +0300

    posting an emai

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,10 +5,11 @@ import urllib.parse
 import urllib.request
 
 
-url, email = sys.argv[1:3]
+if __name__ == '__main__':
+    url, email = sys.argv[1:3]
 
-data = urllib.parse.urlencode({'email': email}).encode('ascii')
-req = urllib.request.Request(url, data)
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
+    req = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
