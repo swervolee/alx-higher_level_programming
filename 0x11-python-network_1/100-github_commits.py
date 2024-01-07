1ab0780251dd44533a1f67e26918385d1ac398f0
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-08 02:49:30 +0300

    correction

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,8 +8,11 @@ if __name__ == '__main__':
     url = f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits'
     headers = {'Accept': 'application/vnd.github+json'}
     r = requests.get(url, headers=headers)
-    for v, i in enumerate(r.json()):
-        print(i.get('commit').get('tree').get('sha') +
-              ': ' + i.get('commit').get('author').get('name'))
-        if v == 10:
-            break
+    comm = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                comm[i].get("sha"),
+                comm[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
