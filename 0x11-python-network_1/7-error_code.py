a31bdb8daa6f503cff39e9fcfe7ca3b7f9d2bfd1
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-07 14:38:19 +0300

    handles status code greater than 399

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     r = requests.get(sys.argv[1])
 
-    if r.status_code >= 404:
+    if r.status_code >= 400:
         print(f'Error code: {r.status_code}')
     else:
         print(r.text)
