deba682c48fe4e22a0129317b5d53c5de944fa26
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-07 23:09:01 +0300

    Json reads

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,19 +4,16 @@ import requests
 import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        q = sys.argv[1]
-    except IndexError as e:
-        q = ""
-
-        url = 'http://0.0.0.0:5000/search_user'
-        r = requests.post(url, data={'q': q})
-
-        if r.status_code == 204:
-            print('No result')
+        response = r.json()
+        if response == {}:
+            print("No result")
         else:
-            try:
-                print(f'[{r.json().get("id")}] {r.json().get("name")}')
-            except Exception:
-                print('Not a valid JSON')
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
