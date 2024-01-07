275515e0d65a50a78250f4371fb59eda909e89f5
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-07 13:06:07 +0300

    Smooth get

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 'reads a Get and handles errors'
-
-
 import urllib.request
 import sys
 from urllib.error import HTTPError
 
-try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.read().decode('utf-8'))
-except HTTPError as e:
-    print(f'Error code: {e.code}')
+
+if __name__ == '__main__':
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print(f'Error code: {e.code}')
