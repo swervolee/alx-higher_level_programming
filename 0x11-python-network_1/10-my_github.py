1fc53061564eeed9d2b26bd23d8290e4a180bf66
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-08 01:43:53 +0300

    Correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,14 +2,15 @@
 'GITHUB RESTFUL API'
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == '__main__':
     username, password = sys.argv[1: 3]
     headers = {'Authorization': 'token ' + str(password)}
-    print(headers)
     url = 'https://api.github.com/user'
+    basic = HTTPBasicAuth(username, password)
 
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, auth=basic)
 
     print(r.json()['id'])
