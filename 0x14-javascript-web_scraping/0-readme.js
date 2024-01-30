2c06f8cf69e843bdac69288e22f4180f24cbe6db
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-30 19:36:54 +0300

    File reading

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,4 +3,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err) => console.error(err));
+fs.readFile(process.argv[2], 'utf-8', (err, inputD) => {
+  if (err) throw err;
+  console.log(inputD.toString());
+});
