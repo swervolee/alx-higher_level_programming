d7bf83effdaeff247b62c16b5d621edef0a92afa
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-30 19:54:45 +0300

    File write correction

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,6 +4,6 @@
 const fs = require('fs');
 
 const path = process.argv[2];
-const data = `${process.argv[3]}\n`;
+const data = process.argv[3];
 
 fs.writeFile(path, data, (err) => { if (err) throw err; });
